a55b3ac51601ad65e28f13bc58803f761f38bf66
bhabani.das@gmail.com <bhabani.das@gmail.com>
2018-04-06 21:33:19 -0700

    before using theming

--- src/app/shell.component.ts
+++ src/app/shell.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
     selector: 'app-shell',
     template: ` <div mdl-shadow="2">
                     <div mdl-layout-fixed-header mdl-layout-header-seamed>
-                        <div class="map-view-header">
+                        <div class="shell-header">
                             <div class="mdl-layout__header-row">
                                 <app-go-home-component class="sns-go-home"></app-go-home-component>
                                 <app-user-information></app-user-information>
